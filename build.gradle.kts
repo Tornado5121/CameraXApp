8ba9a80b0da369e3c28b0e0f616834a7906a8498
Tornado5121 <zhadkoroman@gmail.com>
2023-01-04 22:54:20 +0200

    refactor gradle groovy to gradle kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.1.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21"
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
-}
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+    extra.apply {
+        set("lifecycle_version", "2.4.1")
+        set("koin_version", "3.2.0-beta-1")
+        set("camerax_version", "1.1.0-beta03")
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "MyCameraApp"
-include ':app'
+include(":app")
